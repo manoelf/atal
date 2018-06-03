aa4f15e0006e8e61bd3595ab33166fea1ee830f6
manoelf <jose.ferreira@ccc.ufcg.edu.br>
2018-06-03 01:29:48 -0300

    E a poliça pego os lardão

--- cops_robbers.py
+++ cops_robbers.py
@@ -1,48 +1,57 @@
 
 
-def find_robbers(maze, x, y):
+def find_robbers(maze, x, y, path):
     '''
-    That maze is a 5x5 matrix
+        That maze is a 5x5 matrix
     '''
-    
-    if (x == 4 and y == 4):
-        return True
-    if (is_safe(maze, x, y)) :
-        if (find_robbers(maze, x + 1, y) == True):
-            return True
-        
-        if (find_robbers(maze, x, y + 1) == True):
-            return True
+    if (is_safe(maze, x, y)):
+        path[x][y] = 1
+        find_robbers(maze, x - 1, y, path)
+        find_robbers(maze, x + 1, y, path)
+        find_robbers(maze, x, y - 1, path)
+        find_robbers(maze, x, y + 1, path)
+      
+    else:
         return False
-    return False
             
 
 
 
 def is_safe(maze, x, y):
-    if (x < 5 and y < 5):
-        if (maze[x][y] == 0):
+    if (x < 5 and y < 5 and x >= 0 and y >= 0):
+        if (maze[x][y] == 0 and path[x][y] == 0 ):
             return True
         else:
             return False
     else:
         return False
         
+def empty_path():
+    path = []
+    for i in range(5):
+        line = []
+        for j in range(5):
+            line.append(0)
+        path.append(line)
+    return path
+
+
+    
 tests = int(input())
 cont = 0
 
 while (cont < tests):
     maze = []
     line = input()
-
+    path = empty_path()
     if (len(line.strip()) > 0):
         cont += 1
         maze.append(list(map(int, line.split())))
         for i in range(4):
             line = input()
             maze.append(list(map(int, line.split())))
-        result = find_robbers(maze, 0, 0)
-        if (result):
+        find_robbers(maze, 0, 0, path)
+        if (path[4][4]):
             print('COPS')
         else:
             print('ROBBERS')
